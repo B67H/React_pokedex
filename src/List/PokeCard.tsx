9885abb3b8451d3fc207b3ada6e625aacd95885e
B6H7 <xyboy9024@gmail.com>
2022-10-09 03:58:11 +0900

    markup2

--- src/List/PokeCard.tsx
+++ src/List/PokeCard.tsx
@@ -1,13 +1,21 @@
 import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
 import PokeMarkChip from "../Common/PokeMarkChip";
 import PokeNameChip from "../Common/PokeNameChip";
 
 const tempImgUrl =
   "https://png.pngitem.com/pimgs/s/46-468761_pikachu-png-transparent-image-pikachu-png-png-download.png";
+
 const PokeCard = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/pokemon/피카츄`);
+  };
+
   return (
     <div>
-      <Item>
+      <Item onClick={handleClick}>
         <Header>
           <PokeNameChip />
         </Header>
@@ -15,7 +23,7 @@ const PokeCard = () => {
           <Image src={tempImgUrl} alt="이상해씨" />
         </Body>
         <Footer>
-          <PokeMarkChip/>
+          <PokeMarkChip />
         </Footer>
       </Item>
     </div>
@@ -33,6 +41,19 @@ const Item = styled.li`
 
   border: 1px solid #c0c0c0;
   box-shadow: 1px 1px 3px 1px #c0c0c0;
+
+  cursor: pointer;
+  transition: transform 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.1)
+  }
+
+  &:active {
+    background-color: yellow;
+    opacity: 0.8;
+    transition: background-color 0s;
+  }
 `;
 
 const Header = styled.section`
@@ -57,7 +78,7 @@ const Image = styled.img`
 const Footer = styled.section`
   display: flex;
   flex-direction: row;
-  margin : 8px 0;
+  margin: 8px 0;
 `;
 
 export default PokeCard;
